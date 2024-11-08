0f98d5b0ddba1ee234f3b18968955d9902c11e29
Pieter Monsieurs <pmonsieurs@itg.be>
2024-11-08 14:41:27 +0100

    large update on ncounter script

--- ncounter_analysis.R
+++ ncounter_analysis.R
@@ -18,9 +18,9 @@ library(openxlsx)
 library(org.Mm.eg.db)
 library(AnnotationDbi)
 library(clusterProfiler)
+library(EnhancedVolcano)
 
-
-
+ 
 #### 1. input data and metadata ####
 
 # ---- ├ 1.1 parameter settings ----
@@ -30,8 +30,9 @@ out_dir = '/Users/pmonsieurs/programming/trypanosoma_rnaseq_bitesite/results/nco
 ## be stored in a tab-delimited file instead of a comma separated file, so 
 ## use the .txt metadata and not the .csv version
 meta_data_dir = '/Users/pmonsieurs/programming/trypanosoma_rnaseq_bitesite/data/'
-meta_data_file = '/Users/pmonsieurs/programming/trypanosoma_rnaseq_bitesite/data/ncounter_metadata.xlsx'
-meta_data = read.xlsx(meta_data_file)
+meta_data_file = '/Users/pmonsieurs/programming/trypanosoma_rnaseq_bitesite/data/ncounter_metadata.txt'
+meta_data_file_xlsx = '/Users/pmonsieurs/programming/trypanosoma_rnaseq_bitesite/data/ncounter_metadata.xlsx'
+meta_data = read.xlsx(meta_data_file_xlsx)
 head(meta_data)
 
 
@@ -181,9 +182,15 @@ pca_data <- cbind.data.frame(pca_data, meta_data_ordered)
 pca_data$batch = as.factor(pca_data$batch)
 
 ggplot(pca_data, aes(x = PC1, y = PC2)) +
-  # geom_point(aes(color = timepoint), size = 3) +
+  geom_point(aes(color = timepoint), size = 3) +
   # geom_point(aes(color = batch), size = 3) +
-  geom_point(aes(color = infection), size = 3) +
+  # geom_point(aes(color = infection), size = 3) +
+  # geom_text(aes(label = rownames(pca_data)), vjust = -0.5, label.size=0, check_overlap = TRUE) +
+  # geom_text(aes(label = rownames(pca_data)), vjust = -0.5) +
+  geom_text_repel(aes(label = rownames(pca_data)),                # Add labels with lines
+                  box.padding = 0.35,                             # Extra space around labels
+                  point.padding = 0.3,                            # Space between points and labels
+                  max.overlaps = Inf) +
   theme_minimal() +
   labs(title = "PCA of Samples", x = "PC1", y = "PC2")
 
@@ -233,7 +240,7 @@ result_12hI =glm.LRT(ns_data, design.full,
 
 
 # ---- ├ 3.2 edgeR ----
-
+## RUVseq
 ## edgeR on normalised data, but edgeR does not except negative
 ## values
 group <- designs$group # Adjust to your conditions
@@ -305,7 +312,85 @@ fit_emma <- eBayes(fit_emma)
 topTable(fit_emma, coef=2)
 topTable(fit_emma, coef=1)
 
+## volcano plots per comparison
+for (coeff in 1:8) {
+  condition = colnames(contrast_matrix)[coeff]
+  condition = gsub("g_", "", condition)
+  condition = gsub(" ", "", condition)
+  
+  diff_expressed = topTable(fit2, coef=coeff, n=1000, p.value=1,lfc=0)
+  diff_expressed = diff_expressed[grep("Endogenous_", rownames(diff_expressed)),]
+  rownames(diff_expressed) = sapply(strsplit(rownames(diff_expressed), "_"), `[`, 2)
 
+  x = EnhancedVolcano(diff_expressed,
+                  rownames(diff_expressed),
+                  x= "logFC",
+                  y="adj.P.Val", 
+                  xlim = c(min(diff_expressed$logFC, na.rm = TRUE) - 0.5, max(diff_expressed$logFC, na.rm = TRUE) + 0.5),
+                  ylim = c(0, max(-log10(diff_expressed$adj.P.Val), na.rm = TRUE) + 0.2),
+                  pCutoff = 5e-02, 
+                  title = condition,
+                  subtitle = "",
+                  titleLabSize = 14,
+                  labSize = 4,
+                  subtitleLabSize = 2,
+                  captionLabSize = 10,
+                  legendPosition = "right",
+                  legendLabels = c("NS", "FC", "Pval", "FC+pval"),
+                  drawConnectors = TRUE,
+                  arrowheads = FALSE
+
+  )
+  
+  png_out_file = paste0(out_dir, "volcano_", condition, ".png")
+  ggsave(png_out_file, plot = p_volcano, width= 10, height=8)
+  
+  
+}
+
+## correlation with the RNA-seq data sets. First read the DESeq2 output, which
+## is derived for infected (different time points) versus 0h
+tissue = 'ear'
+src_dir = '/Users/pmonsieurs/programming/trypanosoma_rnaseq_bitesite/results/deseq2/'
+excel_file = paste0(src_dir, 'rnaseq_immune_response.',tissue,'.timeseries.xlsx')
+deseq_data = read.xlsx(excel_file)
+
+count = 0
+plot_list = list()
+for (timepoint in c("4h", "12h", "24h", "96h")) {
+  column_name_deseq = paste0(timepoint, "_fc")
+  column_name_limma = paste0("g_", timepoint,"_I - g_0h_I")
+  
+  ## get RNA-seq data
+  rnaseq_data = deseq_data[, c('external_gene_name', column_name_deseq)]
+  
+  ## get nCounter data
+  ncounter_list = fit2$coefficients[, column_name_limma]
+  ncounter_list = ncounter_list[grep("Endogenous", names(ncounter_list))]
+  ncounter_data = as.data.frame(ncounter_list)
+  rownames(ncounter_data) = sapply(strsplit(rownames(ncounter_data), "_"), `[`, 2)
+  rnaseq_sorted = rnaseq_data[match(rownames(ncounter_data), rnaseq_data$external_gene_name),]
+  dim(rnaseq_sorted)
+  dim(ncounter_data)
+  plot_data = cbind.data.frame(rnaseq_sorted, ncounter_data)
+  colnames(plot_data) = c('gene_id', 'rnaseq_fc', 'ncounter_fc')
+  
+  ## calculate and plot correlation
+  cor_value = cor(plot_data[,2], plot_data[,3], use="complete.obs")
+  
+  p = ggplot(plot_data, aes(x = rnaseq_fc, y = ncounter_fc))    
+  p = p + geom_point(alpha=0.3, size=1.5, color="black")
+  p = p + geom_smooth(method = "lm", se=FALSE, color="blue", formula = y ~ x) 
+  p = p + theme_bw()
+  # p = p + stat_cor(aes(label = ..rr.label..), color = "red", geom = "label")
+  p = p + stat_cor(method = "pearson", label.x = 0, label.y = 6, color="red")
+  p = p + ggtitle(timepoint)
+  p
+  
+  plot_list[[timepoint]] = p
+}
+
+wrap_plots(plot_list, ncol=2)
 
 #### 4. GSEA analysis ####
 
@@ -381,7 +466,7 @@ for (coeff in c(2,4,6,8)) {
 
   
   
-  # ---- 4.2 using categories of Nanostring using hypergeometric distribution ----
+# ---- 4.2 using categories of Nanostring using hypergeometric distribution ----
   
 
 ## To Do ##
